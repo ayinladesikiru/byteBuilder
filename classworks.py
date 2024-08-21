b8356fbb725511be30984465cd7ac2b0dd63503f
ayinlade sikiru <ayinladesikiru@gmail.com>
2024-08-21 16:41:47 +0100

    compute area of a circle

---
+++ classworks.py
@@ -0,0 +1,22 @@
+''' calculate area of a square
+hdsfjdkglgljgflfdldflsd
+jdfjfusksdss
+'''
+
+name = "emmanuel"
+my_name = "sikiru"
+gender = 'm'
+print(type(gender))
+
+age = 21
+new_age = age + 4
+print(new_age) 
+age = 21.5
+
+print("the length is", len(my_name))
+
+# this is a single line comment
+
+
+print(name, my_name, age, "williams")
+
